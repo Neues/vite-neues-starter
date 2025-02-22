2ad59b762834f43134d7c2200d737b1548afbd9f
Noah Sgorbati <noahsgorbati@gmail.com>
2025-02-22 01:59:25 +0000

    fix prettier step in lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "!**/*": "npm run prettier",
+  "*": "npm run prettier",
   "*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}": ["npm run lint"],
 };
